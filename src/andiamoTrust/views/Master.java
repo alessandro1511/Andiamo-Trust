458a6b646ccb72d1c6b08c06eb9a9ee11b87ca51
ac <alessandro1511@gmail.com>
2017-01-24 20:33:23 +0100

    inserita la configurazione del log

--- src/andiamoTrust/views/Master.java
+++ src/andiamoTrust/views/Master.java
@@ -2,12 +2,18 @@ package andiamoTrust.views;
 
 import java.awt.EventQueue;
 
-public abstract class Master {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+public abstract class Master {
+	
+	private static final Logger logger = LogManager.getLogger(Master.class);
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					getLogger().info("Login view");
 					Login frame = new Login();
 					frame.setVisible(true);
 				} catch (Exception e) {
@@ -15,5 +21,9 @@ public abstract class Master {
 				}
 			}
 		});
+	}
+
+	public static Logger getLogger() {
+		return logger;
 	}
 }
